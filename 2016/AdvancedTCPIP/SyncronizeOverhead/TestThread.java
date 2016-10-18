292a92cce9002b99b32381c5950c6328705f015c
munhyunsu <munhyunsu@gmail.com>
2016-10-18 21:49:47 +0900

    동기화 오버헤드

---
+++ 2016/AdvancedTCPIP/SyncronizeOverhead/TestThread.java
@@ -0,0 +1,62 @@
+class PrintDemo {
+   public void printCount() {
+      try {
+        for(int k = 2147483647/20000; k > 0; k--) {
+        for(int j = 2147483647/20000; j > 0; j--) {
+         for(int i = 2147483647/20000; i > 0; i--) {
+            //System.out.println("Counter   ---   "  + i );
+         }
+         }
+         }
+      }catch (Exception e) {
+         System.out.println("Thread  interrupted.");
+      }
+   }
+}
+
+class ThreadDemo extends Thread {
+   private Thread t;
+   private String threadName;
+   PrintDemo  PD;
+
+   ThreadDemo( String name,  PrintDemo pd) {
+      threadName = name;
+      PD = pd;
+   }
+   
+   public void run() {
+//      synchronized(PD) {
+         PD.printCount();
+//      }
+      System.out.println("Thread " +  threadName + " exiting.");
+   }
+
+   public void start () {
+      System.out.println("Starting " +  threadName );
+      if (t == null) {
+         t = new Thread (this, threadName);
+         t.start ();
+      }
+   }
+}
+
+public class TestThread {
+
+   public static void main(String args[]) {
+      PrintDemo PD = new PrintDemo();
+
+      ThreadDemo T1 = new ThreadDemo( "Thread - 1 ", PD );
+      ThreadDemo T2 = new ThreadDemo( "Thread - 2 ", PD );
+
+      T1.start();
+      T2.start();
+
+      // wait for threads to end
+      try {
+         T1.join();
+         T2.join();
+      }catch( Exception e) {
+         System.out.println("Interrupted");
+      }
+   }
+}
